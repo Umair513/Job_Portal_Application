4c6e2a94ad0a11d4f36b06e5feaf6cd9abca8c0e
Umair513 <soomroumair513@gmail.com>
2024-03-01 17:45:10 +0500

    Created User Schema Model

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
+import testRoutes from "./routes/testRoutes.js"
+import cors from "cors"
+import morgan from "morgan"
 
 dotenv.config()
 
 connectDB()
 const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
 
-app.get("/", (req, res) => {
-    res.send("<h1>Welcome to Job Portal</h1>")
-})
+app.use("/api/v1/test",testRoutes)
 
 const PORT = process.env.PORT || 8080
 const MODE = process.env.DEV_MODE || "development"
