7f985a079a987e44646cac68be6d6a95a5b405e8
Umair513 <soomroumair513@gmail.com>
2024-03-07 00:55:41 +0500

    Job update and delete API

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -17,3 +17,41 @@ export const getAllJobsController =async (req, res, next) => {
         jobs
     })
 }
+
+export const updateJobController = async(req, res, next) => {
+    const {id} = req.params
+    // if(id){
+    //     next(`id getted from req.params is ${id}`)
+    // }
+    const {company, position} = req.body
+    if(!company || !position){
+        next("please provide all fields")
+    }
+    const job = await jobsModel.findOne({_id: id})
+    if(!job){
+        next(`no jobs found with this id ${id}`)
+    }
+    if(req.user.userId !== job.createdBy.toString()){
+        // return
+        next("you are not authorized to update this job")
+    }
+    const updateJob = await jobsModel.findOneAndUpdate({_id:id}, req.body, {
+        new:true,
+        runValidators:true
+    })
+    res.status(200).json({updateJob})
+}
+
+
+export const deleteJobController = async(req, res, next) => {
+    const {id} = req.params
+    const job = await jobsModel.findOne({_id: id})
+    if(!job){
+        next(`no jobs found with this id ${id}`)
+    }
+    if(!req.user.userId === job.createdBy.toString()){
+        next("you are not authorized to delete this job")
+    }
+    await job.deleteOne()
+    res.status(200).json({message: "job deleted successfully"})
+}
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,9 +1,11 @@
 import express from "express"
 import userAuth from "../middlewares/authMiddleware.js"
-import { createJobController, getAllJobsController } from "../controllers/jobsController.js"
+import { createJobController, deleteJobController, getAllJobsController, updateJobController } from "../controllers/jobsController.js"
 
 const router = express.Router()
 
 router.post("/create-job", userAuth, createJobController)
 router.get("/get-job",userAuth,getAllJobsController)
+router.patch("/update-job/:id", userAuth , updateJobController)
+router.delete("/delete-job/:id", userAuth,deleteJobController)
 export default router
