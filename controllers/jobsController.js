4e2252c33259c8e0e1a222b5e447bcbabbeb2921
Umair513 <soomroumair513@gmail.com>
2024-03-09 01:16:59 +0500

    Stats Filters

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,4 +1,6 @@
 import jobsModel from "../models/jobsModel.js"
+import mongoose from "mongoose"
+import moment from "moment"
 
 export const createJobController = async (req, res, next) => {
     const {company, position} = req.body
@@ -54,4 +56,52 @@ export const deleteJobController = async(req, res, next) => {
     }
     await job.deleteOne()
     res.status(200).json({message: "job deleted successfully"})
+}
+
+export const jobStatsController =async (req, res) => {
+    const stats = await jobsModel.aggregate([
+        {
+            $match:{
+                createdBy: new mongoose.Types.ObjectId(req.user.userId)
+            },
+            
+        },
+        {
+            $group: {
+                _id: "$status",
+                count: { $sum: 1 }
+            }
+        }
+    ])
+    const defaultStats = {
+        pending: stats.pending || 0,
+        reject: stats.reject || 0,
+        interview: stats.interview || 0,
+    }
+
+    let monthlyApplication = await jobsModel.aggregate([
+        {
+            $match:{
+                createdBy: new mongoose.Types.ObjectId(req.user.userId)
+            }
+        },
+        {
+            $group:{
+                _id:{
+                    year:{$year:"$createdAt"},
+                    month:{$month:"$createdAt"}
+                },
+                count:{
+                    $sum:1
+                }
+            }
+        }
+    ])
+
+    monthlyApplication = monthlyApplication.map(item => {
+       const {_id:{year, month}, count} = item
+       const date = moment().month(month - 1).year(year).format("MMM YYYY")
+       return {date, count}
+    }).reverse()
+    res.status(200).json({totalJobs:stats.length, defaultStats, monthlyApplication})
 }
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import userAuth from "../middlewares/authMiddleware.js"
-import { createJobController, deleteJobController, getAllJobsController, updateJobController } from "../controllers/jobsController.js"
+import { createJobController, deleteJobController, getAllJobsController, jobStatsController, updateJobController } from "../controllers/jobsController.js"
 
 const router = express.Router()
 
@@ -8,4 +8,5 @@ router.post("/create-job", userAuth, createJobController)
 router.get("/get-job",userAuth,getAllJobsController)
 router.patch("/update-job/:id", userAuth , updateJobController)
 router.delete("/delete-job/:id", userAuth,deleteJobController)
+router.get("/job-stats", userAuth, jobStatsController)
 export default router
