b3bfcb5fc3becf6e33024202b33b33972726520a
Umair513 <soomroumair513@gmail.com>
2024-03-11 00:42:35 +0500

    some security fixes using different npm packages

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import { loginController, registerController } from '../controllers/authController.js';
+import rateLimit from 'express-rate-limit';
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    standardHeaders: true,
+    legacyHeaders: false
+  });
 const router = express.Router();
 
-router.post("/register",registerController)
-router.post("/login",loginController)
+router.post("/register",limiter,registerController)
+router.post("/login",limiter,loginController)
 
 export default router;
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ import morgan from "morgan"
 import errorMiddleware from "./middlewares/errorMiddleware.js"
 import userRoutes from "./routes/userRoutes.js"
 import jobsRoute from "./routes/jobsRoute.js"
+import helmet from "helmet"
+import xss from "xss-clean"
+import mongoSanitize from "express-mongo-sanitize"
 
 dotenv.config()
 
@@ -17,7 +20,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
-
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 app.use("/api/v1/test",testRoutes)
 app.use("/api/v1/auth", authRoute)
 app.use("/api/v1/user", userRoutes)
