bf064a0bf4a4d6c5593b8ee7b4c860e63e6e6edd
Umair513 <soomroumair513@gmail.com>
2024-03-13 23:52:01 +0500

    Routing and CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Job Portal Application</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage></HomePage>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+    </>
   );
 }
 
