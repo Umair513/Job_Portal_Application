f37afdeb3aa37beb8dd12fe6a8bde16edc831366
Umair513 <soomroumair513@gmail.com>
2024-03-14 12:06:16 +0500

    Register Form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,102 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
-  )
-}
+  const [values, setValues] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
 
-export default Register
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setValues({
+      ...values,
+      [e.target.name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      console.log(values);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="form-container">
+        <h1 className="mb-5">Registration Form</h1>
+        <form className="card p-5" onSubmit={handleSubmit}>
+          <div className="mb-1">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-1">
+            <label htmlFor="lastName" className="form-label">
+              Last Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="lastName"
+              value={values.lastName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-1">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-1">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-1">
+            <label htmlFor="location" className="form-label">
+              Location
+            </label>
+            <input type="text" className="form-control" name="location" />
+          </div>
+          <div className="d-flex justify-space-between">
+            <p>
+              Already Registered? <Link to="/login">Login</Link>
+            </p>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
